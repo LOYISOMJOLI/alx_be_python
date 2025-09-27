5069d453a2b22bf48d606b67d79d6bf5754637b2
LOYISO MJOLI <loyisovctr@gmail.com>
2025-09-27 23:38:35 +0200

    addsdsf

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,11 @@
+from datetime import date
+from datetime import timedelta
+
+def display_current_datetime:
+    current_date = date.now()
+    print("Current date and time:", current_date)
+    numOfDays =int( input("Enter the number of days to add to the current date:"))
+    future_date = current_date.today() + timedelta(numOfDays)
+    print("future_date:",future_date)
+
+display_current_datetime()

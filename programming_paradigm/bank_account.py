e49de0e092f5e4f263eeb7d39bc0af7b784a1dfc
LOYISO MJOLI <loyisovctr@gmail.com>
2025-10-05 08:08:41 +0200

    Programming paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = float(account_balance)
+
+#This function is for depositing the balance
+    def deposit(self,amount):
+        self.account_balance += amount
+
+# This function if for withdrawing the balance
+    def withdraw(self,amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            print("Insufficient funds")
+
+#This function is for displaying the baalance
+    def display_balance(self):
+        print(f"Current balance: {self.account_balance}")
